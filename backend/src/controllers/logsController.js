32629f51b22bef0bde07692a41984587d2932136
tylerle29 <tylerle29@gmail.com>
2025-09-26 14:49:36 -0500

    logs created

--- backend/src/controllers/logsController.js
+++ backend/src/controllers/logsController.js
@@ -1,9 +1,29 @@
-export function getAllLogs(req, res) {
-    res.status(200).send("Fetched # logs");
+import Log from "../models/Log.js";
+
+export async function getAllLogs(req, res) {
+    try {
+        const logs = await Log.find();
+        res.status(200).json(logs);
+    }
+    catch (error) {
+        console.error("Error fetching logs:", error);
+
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
-export function createLog(req, res) {
-    res.status(201).json({ message: "Log created successfully"});
+export async function createLog(req, res) {
+    try {
+        const { title, content, tags } = req.body;
+        const newLog = new Log({ title, content, tags });
+        console.log(title, content, tags);
+        await newLog.save();
+        res.status(201).json({ message: "Log created successfully", log: newLog });
+    }
+    catch (error) {
+        console.error("Error creating log:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 export function updateLog(req, res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,13 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+// Middleware
+app.use(express.json());
 app.use("/api/logs", logsRoutes);
 console.log('running server.js');
 
 
 
-app.listen(PORT, () => {
-  console.log('Server running at http://localhost:', PORT, '/');
+app.listen(parseInt(PORT, 10), () => {
+  console.log('Server running at http://localhost:',PORT, '/');
 });
 
 //mongodb+srv://tylerle3_db_user:<db_password>@cluster0.8utcpzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
