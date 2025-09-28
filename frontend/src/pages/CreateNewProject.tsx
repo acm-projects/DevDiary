8ee1b8095b09a96a6b0f62402a431e36a96eece4
JMANY42 <2187.j.man@gmail.com>
2025-09-28 18:53:59 -0500

    added react router + reorganized file structure

---
+++ frontend/src/pages/CreateNewProject.tsx
@@ -0,0 +1,12 @@
+import '../styles/App.css'
+
+function CreateNewProject() {
+  return (
+    <div className="w-screen h-screen bg-center bg-cover bg-[url(src/assets/background.png)]">      
+      <h1>Create New Project</h1>
+      <p>work in progress</p>
+    </div>
+  )  
+}
+
+export default CreateNewProject
