88f9448c5e28940f0529ac13dc9b3cf95b5505bd
JMANY42 <2187.j.man@gmail.com>
2025-10-08 02:46:28 -0500

    fixed formatting

--- frontend/src/components/Search/SearchBar.tsx
+++ frontend/src/components/Search/SearchBar.tsx
@@ -1,35 +1,34 @@
 import '/src/styles/App.css'
-import SearchIcon from "/src/assets/icons/search.png"
 
 function SearchBar() {
     return (
-        <div className="">
-            <form className="flex justify-end items-center relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <div className="">
+        <form className="flex justify-end items-center relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20">
-                <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-            </svg>
-            </div>
-                <input
-                    className="bg-[#011522] px-3 py-1 placeholder-gray-400 placeholder:font-headvig font-headvig text-white text-lg w-full rounded-lg border-white pl-10"
-                    type="text" 
-                    id="search" 
-                    name="search" 
-                    placeholder="Search..."
-                />
-            </form>
-        </div>
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20">
+              <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            /></svg>
+          </div>
+
+          <input
+            className="bg-[#011522] px-3 py-1 placeholder-gray-400 placeholder:font-headvig font-headvig text-white text-lg w-full rounded-lg border-white pl-10"
+            type="text" 
+            id="search" 
+            name="search" 
+            placeholder="Search..."
+          />
+        </form>
+      </div>
     );
 }
 
